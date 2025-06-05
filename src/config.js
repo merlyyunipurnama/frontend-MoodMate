13c50ed68cea9b33a2a33837ed3d36cfbae6e900
Merly Yuni Purnama <129154889+merlyyunipurnama@users.noreply.github.com>
2025-06-05 13:31:20 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
+const CONFIG = {
+  BASE_URL: "http://localhost:9000/api",
+  DEFAULT_LANGUAGE: "id-ID",
+  CACHE_NAME: "MoodMate-V1",
+  DATABASE_NAME: "moodmate-database",
+  DATABASE_VERSION: 1,
+  WEB_SOCKET_SERVER: "ws://localhost:9000",
+};
+
+export default CONFIG;
 
